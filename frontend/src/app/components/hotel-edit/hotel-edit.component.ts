89635423ddad26f764c7bd2d803713bab4aec1fd
saka <sascha.kaufmann@zuehlke.com>
2016-06-07 11:43:39 +0200

    frontend: Add initial version of edit hotel component

---
+++ frontend/src/app/components/hotel-edit/hotel-edit.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {Hotel, Country} from '../../model/backend-typings';
+import {HotelService} from '../../shared/hotel.service';
+import {RouteParams, Router} from '@ngrx/router';
+import {CountryService} from '../../shared/country.service';
+import {MaterializeDirective} from 'angular2-materialize';
+import {Observable} from 'rxjs/Observable';
+
+@Component({
+  selector: 'hotel-edit',
+  directives: [MaterializeDirective],
+  providers: [HotelService, CountryService],
+  template: require('./hotel-edit.component.html')
+})
+export class HotelEditComponent implements OnInit {
+  hotelId: Observable<number>;
+  hotel: Hotel;
+  countries: Observable<Country[]>;
+
+  constructor(private hotelService: HotelService,
+              private countryService: CountryService,
+              private router: Router,
+              routeParams: RouteParams) {
+    this.hotel = {} as Hotel;
+    this.hotelId = routeParams.pluck<number>('id');
+  }
+
+  ngOnInit() {
+    this.hotelId.flatMap(id => this.hotelService.getHotelAuthenticated(id)).subscribe(hotel => {
+      this.hotel = hotel;
+    });
+    this.countries = this.countryService.getCountriesAuthenticated();
+  }
+
+  saveHotel() {
+    this.hotelService.saveHotel(this.hotel).subscribe(data =>
+      this.openHotel(data));
+  }
+
+  openHotel(hotel: Hotel) {
+    this.router.go('/hotels/' + hotel.id);
+  }
+}
