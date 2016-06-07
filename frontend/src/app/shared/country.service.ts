72223425073dad2464de86e48c3f8dbc645cbf75
Michael Werner <michael.werner@windowslive.com>
2016-06-07 12:00:00 +0200

    Countries fetched from restcountries.eu service now - Needed to unpack Observable on controller, otherwise calls get executed twice

--- frontend/src/app/shared/country.service.ts
+++ frontend/src/app/shared/country.service.ts
@@ -1,15 +1,30 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/toArray';
 import {Country} from '../model/backend-typings';
-import {CrudService} from './crud.service';
 
 @Injectable()
 export class CountryService {
 
-  constructor(private crud: CrudService) {
+  private baseUrl:string = "https://restcountries.eu/rest/v1";
+
+  constructor(private http:Http) {
   }
 
-  getCountriesAuthenticated(): Observable<Country[]> {
-    return this.crud.get('/rest/countries');
+  getCountriesAuthenticated():Observable<Country[]> {
+    return this.http.get(this.baseUrl + '/all')
+      .map(response => response.json())
+      .flatMap(countries => Observable.from(countries))
+      .map(country => {
+          return {
+            name: country.name,
+            code: country.alpha2Code
+          } as Country;
+        }
+      )
+      .toArray();
   }
 }
