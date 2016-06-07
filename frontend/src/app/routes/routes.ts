a1afd7ddff82e7edc1cdcff91e5780ce16ba8db5
danielsuter <suter_daniel@hotmai.com>
2016-06-07 16:00:58 +0200

    Stubs for mail templates.

--- frontend/src/app/routes/routes.ts
+++ frontend/src/app/routes/routes.ts
@@ -7,6 +7,7 @@ import {HotelNewComponent} from "../components/hotel-new/hotel-new.component";
 import {CampsComponent} from "../components/camp-overview/camps.component";
 import {CampNewComponent} from "../components/camp-new/camp-new.component";
 import {HotelEditComponent} from "../components/hotel-edit/hotel-edit.component";
+import {MailTemplatesComponent} from "../components/mail-templates/mailtemplates.component";
 
 export const routes: Routes = [
   {
@@ -36,6 +37,11 @@ export const routes: Routes = [
     path: '/camps/new',
     guards: [AuthGuard],
     component: CampNewComponent
+  },
+  {
+    path: '/mailtemplates',
+    guards: [AuthGuard],
+    component: MailTemplatesComponent
   },
   {
     path: '/hotels/edit/:id',
