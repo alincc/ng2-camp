a985a7f35eb492ed2e97c87146f769f50ec28d0f
danielsuter <suter_daniel@hotmai.com>
2016-06-07 12:14:40 +0200

    Bootstrapped camp overview.

--- frontend/src/app/routes/routes.ts
+++ frontend/src/app/routes/routes.ts
@@ -1,9 +1,10 @@
-import {Routes, Route} from '@ngrx/router';
-import {HotelsComponent} from '../components/hotel-overview/hotels.component';
-import {HotelDetailComponent} from '../components/hotel-detail/hotel-detail.component';
-import {LoginComponent} from '../components/login/login.component';
-import {AuthGuard} from './authGuard';
-import {HotelNewComponent} from '../components/hotel-new/hotel-new.component';
+import {Routes, Route} from "@ngrx/router";
+import {HotelsComponent} from "../components/hotel-overview/hotels.component";
+import {HotelDetailComponent} from "../components/hotel-detail/hotel-detail.component";
+import {LoginComponent} from "../components/login/login.component";
+import {AuthGuard} from "./authGuard";
+import {HotelNewComponent} from "../components/hotel-new/hotel-new.component";
+import {CampsComponent} from "../components/camp-overview/camps.component";
 
 export const routes: Routes = [
   {
@@ -23,6 +24,11 @@ export const routes: Routes = [
     path: '/hotels/:id',
     guards: [AuthGuard],
     component: HotelDetailComponent
+  },
+  {
+    path: '/camps',
+    guards: [AuthGuard],
+    component: CampsComponent
   },
   {
     path: '/login',
