1db3a2c5ccb1af082f086d3579c996353d580c9a
danielsuter <suter_daniel@hotmai.com>
2016-06-08 15:55:14 +0200

    Recreate all tables.

--- backend/src/main/resources/db/migration/postgres/V2__all_tables.sql
+++ backend/src/main/resources/db/migration/postgres/V2__all_tables.sql
@@ -1,3 +1,4 @@
+
     create table camp (
         id int8 not null,
         from_date date,
@@ -9,9 +10,9 @@
         primary key (id)
     );
 
-    create table camp_offers (
+    create table camp_offer_requests (
         camp_id int8 not null,
-        offers_id int8 not null
+        offer_requests_id int8 not null
     );
 
     create table hotel (
@@ -19,7 +20,7 @@
         city varchar(255),
         contact_email varchar(255),
         country_code varchar(255),
-        description varchar(255),
+        description text,
         holiday_check_url varchar(255),
         name varchar(255),
         picture_url varchar(255),
@@ -40,7 +41,8 @@
     create table mail_template (
         id int8 not null,
         language varchar(255),
-        text varchar(255),
+        name varchar(255),
+        text text,
         primary key (id)
     );
 
@@ -48,27 +50,27 @@
         id int8 not null,
         accepted boolean not null,
         currency varchar(255),
-        description varchar(255),
+        description text,
         double_rooms int4,
-        expiration_date date,
-        from_date date,
+        expiration_date timestamp,
+        from_date timestamp,
         number_of_people int4,
-        offer_date date,
+        offer_date timestamp,
         single_rooms int4,
-        to_date date,
+        to_date timestamp,
         total_price float8,
         user_id varchar(255),
-        camp_id int8,
         hotel_id int8 not null,
         primary key (id)
     );
 
     create table offer_request (
         id int8 not null,
+        comment text,
         date timestamp,
         last_status_change date,
         status varchar(255),
-        hotel_id int8,
+        offer_id int8,
         primary key (id)
     );
 
@@ -82,50 +84,45 @@
         primary key (id)
     );
 
-    alter table camp_offers
-        add constraint UK_o5krel5d0ingi0jtij2gyct23 unique (offers_id);
+    alter table camp_offer_requests 
+        add constraint UK_2yn7b27it2b93w9y756y0g7y7 unique (offer_requests_id);
 
-    alter table hotel_offers
+    alter table hotel_offers 
         add constraint UK_jq8c8kaurrm3g4fryiqpgpl0s unique (offers_id);
 
-    alter table camp_offers
-        add constraint FK_o5krel5d0ingi0jtij2gyct23
-        foreign key (offers_id)
-        references offer;
+    alter table camp_offer_requests 
+        add constraint FK_2yn7b27it2b93w9y756y0g7y7 
+        foreign key (offer_requests_id) 
+        references offer_request;
 
-    alter table camp_offers
-        add constraint FK_pyh5sfk6d9qpkqnegg1gfj83t
-        foreign key (camp_id)
+    alter table camp_offer_requests 
+        add constraint FK_1jrfhen8y0oqmgspi5aotbfsb 
+        foreign key (camp_id) 
         references camp;
 
-    alter table hotel_offers
-        add constraint FK_jq8c8kaurrm3g4fryiqpgpl0s
-        foreign key (offers_id)
+    alter table hotel_offers 
+        add constraint FK_jq8c8kaurrm3g4fryiqpgpl0s 
+        foreign key (offers_id) 
         references offer;
 
-    alter table hotel_offers
-        add constraint FK_qlnlnl66170wfie02qlbipi13
-        foreign key (hotel_id)
+    alter table hotel_offers 
+        add constraint FK_qlnlnl66170wfie02qlbipi13 
+        foreign key (hotel_id) 
         references hotel;
 
-    alter table offer
-        add constraint FK_dvjbd8or2xrady0rxgul5mmoi
-        foreign key (camp_id)
-        references camp;
-
-    alter table offer
-        add constraint FK_7w0lpk0dph8bfchqetb2x2qna
-        foreign key (hotel_id)
+    alter table offer 
+        add constraint FK_7w0lpk0dph8bfchqetb2x2qna 
+        foreign key (hotel_id) 
         references hotel;
 
-    alter table offer_request
-        add constraint FK_gka6ghev5hyp1qs1exgrr1c9n
-        foreign key (hotel_id)
-        references hotel;
+    alter table offer_request 
+        add constraint FK_5tdvpuno0gs8wmxp5m6alxy9j 
+        foreign key (offer_id) 
+        references offer;
 
-    alter table rating
-        add constraint FK_ns8b94vlj2d1ply4qa5dblcbm
-        foreign key (hotel_id)
+    alter table rating 
+        add constraint FK_ns8b94vlj2d1ply4qa5dblcbm 
+        foreign key (hotel_id) 
         references hotel;
 
     create sequence hibernate_sequence;
