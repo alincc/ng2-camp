c6a4bebe149891f7db895223ebc162db7685dea2
Thomas Schild <thomas@schilds.net>
2016-06-08 11:08:16 +0200

    gonna handout selected hotel form hotel-detail to hotel-edit comp instead of reloading hotel via service - ongoing

--- frontend/src/app/components/hotel-edit/hotel-edit.component.ts
+++ frontend/src/app/components/hotel-edit/hotel-edit.component.ts
@@ -13,7 +13,7 @@ import {Observable} from 'rxjs/Observable';
   template: require('./hotel-edit.component.html')
 })
 export class HotelEditComponent implements OnInit {
-
+  // TODO: get hotel via input instead of loading it anew via id
   @Input('hotel') hotelInput: Hotel;
   hotel: Hotel;
   hotelId: Observable<number>;
@@ -31,14 +31,12 @@ export class HotelEditComponent implements OnInit {
       .getAllCountries()
       .subscribe((countries:Country[]) => { this.countries = countries; });
     this.hotel = (this.hotelInput) ? this.hotelInput : {};
-    /*
     // look for 'id' in path params, and if it is a number go and fetch according hotel from hotelService
     // do not do that if id is not a number, i.e. stick with empty hotel instance
     this.hotelId
       .filter(id => !isNaN(id))
       .flatMap(id => this.hotelService.getHotel(id))
       .subscribe(hotel => { this.hotel = hotel; });
-      */
   }
 
   saveHotel() {
