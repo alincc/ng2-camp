ed5aed53972ce2fc2db7c95d309bfedbc449509a
Thomas Schild <thomas@schilds.net>
2016-06-08 09:26:51 +0200

    ongoing - just use one single hotel-edit component instead of hotel-edit AND hotel-new component

--- frontend/src/app/components/hotel-edit/hotel-edit.component.ts
+++ frontend/src/app/components/hotel-edit/hotel-edit.component.ts
@@ -25,12 +25,15 @@ export class HotelEditComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.countryService.getAllCountries().subscribe((countries:Country[]) =>{
-      this.countries = countries;
-    });
-    this.hotelId.flatMap(id => this.hotelService.getHotel(id)).subscribe(hotel => {
-      this.hotel = hotel;
-    });
+    this.countryService
+      .getAllCountries()
+      .subscribe((countries:Country[]) => { this.countries = countries; });
+    // look for 'id' in path params, and if it is a number go and fetch according hotel from hotelService
+    // do not do that if id is not a number, i.e. stick with empty hotel instance
+    this.hotelId
+      .filter(id => !isNaN(id))
+      .flatMap(id => this.hotelService.getHotel(id))
+      .subscribe(hotel => { this.hotel = hotel; });
   }
 
   saveHotel() {
