e2fcce8ed8eb8f662695e24b6a949831b5c9766d
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-06-08 09:57:55 +0200

    Added offer service for frontend.

---
+++ frontend/src/app/shared/offer.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {Offer} from '../model/backend-typings';
+import {CrudService} from './crud.service';
+
+@Injectable()
+export class OfferService {
+
+  constructor(private crud: CrudService) {
+  }
+
+  saveOffer(offer: Offer): Observable<Offer> {
+    return this.crud.post('/rest/offers', offer);
+  }
+
+  getOffers(): Observable<Offer[]> {
+    return this.crud.get('/rest/offers');
+  }
+
+  getOffer(id: number): Observable<Offer> {
+    return this.crud.get('/rest/offers/' + id);
+  }
+
+  deleteOffer(id: number): Observable<Response> {
+    return this.crud.doDelete('/rest/offers/' + id);
+  }
+}
