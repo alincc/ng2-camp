2c935b25113b20356aaaa36dee5b027e534eba35
Joachim <joachim.prinzbach@gmail.com>
2016-06-27 14:25:45 +0200

    Fix for localhost URL. Closes #12. (#27)

--- frontend/src/app/shared/urlProvider.ts
+++ frontend/src/app/shared/urlProvider.ts
@@ -4,8 +4,10 @@ import {Injectable} from '@angular/core';
 export class UrlProvider {
   public static getBackendUrl(path: string): string {
     let host: string = window.location.hostname;
-    if (host === 'localhost' || host === '127.0.0.1') {
+    if (host === 'localhost') {
       return 'http://localhost:8080' + path;
+    } else if (host === '127.0.0.1') {
+      return 'http://127.0.0.1:8080' + path;
     }
     return path;
   }
