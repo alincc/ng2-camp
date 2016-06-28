f594bc1bcffa847791decaf74fda0390cd4470d7
marco <marcosteybe@users.noreply.github.com>
2016-06-28 10:31:27 +0200

    Feature/user dropdown (#32)
    * add user dropdown in header with logout link
    
    * some styling for user dropdown

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../shared/auth.service';
+import {MaterializeDirective} from "angular2-materialize/dist/index";
 
 @Component({
   selector: 'navigation',
-  directives: [],
+  directives: [MaterializeDirective],
   providers: [],
   template: require('./navigation.component.html')
 })
