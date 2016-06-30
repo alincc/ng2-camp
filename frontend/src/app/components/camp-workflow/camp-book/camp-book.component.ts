632db47a685d7043cc3efe3eeac51562ad6a66a2
Marco Steybe <sty@zuehlke.com>
2016-06-30 15:52:39 +0200

    implement workflow step for selecting hotel

--- frontend/src/app/components/camp-workflow/camp-book/camp-book.component.ts
+++ frontend/src/app/components/camp-workflow/camp-book/camp-book.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {Camp} from '../../../model/backend-typings';
+import {MaterializeDirective} from 'angular2-materialize/dist/index';
 
 @Component({
   selector: 'camp-book',
-  directives: [],
+  directives: [MaterializeDirective],
   template: require('./camp-book.component.html')
 })
 export class CampBookComponent {
