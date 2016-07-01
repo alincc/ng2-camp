f6e8b5a823781fc885450510c0151ea223c6cb58
Marco Steybe <sty@zuehlke.com>
2016-07-01 08:50:52 +0200

    improve offers

--- frontend/src/app/components/camp-workflow/camp-offers/camp-offers.component.ts
+++ frontend/src/app/components/camp-workflow/camp-offers/camp-offers.component.ts
@@ -1,10 +1,12 @@
 import {Component, Input} from '@angular/core';
-import {Camp} from '../../../model/backend-typings';
+import {Camp, Offer} from '../../../model/backend-typings';
 import {MaterializeDirective} from 'angular2-materialize/dist/index';
+import {OfferEditComponent} from '../../offer-edit/offer-edit.component';
+import {MailTemplatesComponent} from '../../mail-templates/mailtemplates.component';
 
 @Component({
   selector: 'camp-offers',
-  directives: [MaterializeDirective],
+  directives: [MaterializeDirective, OfferEditComponent, MailTemplatesComponent],
   template: require('./camp-offers.component.html')
 })
 export class CampOffersComponent {
